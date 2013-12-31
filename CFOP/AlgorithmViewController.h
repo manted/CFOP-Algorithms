240f00386e24f553646dbfb293dd381e8dd8a0db
Ye Hua <mantedh@student.unimelb.edu.au>
2013-12-31 18:37:41 +0800

    add images

---
+++ CFOP/AlgorithmViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlgorithmViewController.h
+//  CFOP
+//
+//  Created by Manted on 31/12/2013.
+//  Copyright (c) 2013 Ye Hua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlgorithmViewController : UIViewController
+
+@end
---
+++ CFOP/F2LViewController.h
@@ -0,0 +1,13 @@
+//
+//  F2LViewController.h
+//  CFOP
+//
+//  Created by Manted on 31/12/2013.
+//  Copyright (c) 2013 Ye Hua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface F2LViewController : UIViewController
+
+@end
